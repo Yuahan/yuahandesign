d96cb060b9b36b654fa559025d71ecc3e8d1654d
yuhan <44660036+Yuahan@users.noreply.github.com>
2019-04-03 20:39:30 +0800

    test

---
+++ social.js
@@ -0,0 +1,10 @@
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+ js.async=true;
+  js.src = "//connect.facebook.net/zh_TW/all.js#xfbml=1&appId= 629937157126585";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
